3ea96986d67bc95c2f572c1b433998fd1231c586
marcialu <yukimiaka@gmail.com>
2017-05-01 21:53:25 +0200

    Adding Port Number

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-//Backend Server File 
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var tasks = require('./routes/tasks');
+
+var port = 3000;
 
 var app = express();
 
