3d0e7872118b5dce9a7f76295c10b7c6289f08a0
marcialu <yukimiaka@gmail.com>
2017-05-01 22:29:49 +0200

    Get all task, Single Task and Save Task

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var mongojs = require('mongojs');
-var db = mongojs('mongodb://mar:mar@ds111718.mlab.com:11718/mytasklis', ['tasks']);
+var db = mongojs('mongodb://brad:brad@ds047666.mlab.com:47666/mytasklist_brad', ['tasks']);
 
+// Get All Tasks
 router.get('/tasks', function (req, res, next) {
     db.tasks.find(function (err, tasks) {
         if (err) {
@@ -10,6 +11,34 @@ router.get('/tasks', function (req, res, next) {
         }
         res.json(tasks);
     });
+});
+
+// Get Single Task
+router.get('/task/:id', function (req, res, next) {
+    db.tasks.findOne({ _id: mongojs.ObjectId(req.params.id) }, function (err, task) {
+        if (err) {
+            res.send(err);
+        }
+        res.json(task);
+    });
+});
+
+//Save Task
+router.post('/task', function (req, res, next) {
+    var task = req.body;
+    if (!task.title || !(task.isDone + '')) {
+        res.status(400);
+        res.json({
+            "error": "Bad Data"
+        });
+    } else {
+        db.tasks.save(task, function (err, task) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(task);
+        });
+    }
 });
 
 module.exports = router;
