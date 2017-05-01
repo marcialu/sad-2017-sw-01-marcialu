7bfa3d9d3ce66fbc77f2eaba4d41510764bf2c30
marcialu <yukimiaka@gmail.com>
2017-05-01 23:02:04 +0200

    App component Task

--- angular-src/src/app/app.component.ts
+++ angular-src/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { TaskService } from './services/task.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  moduleId: module.id,
+    selector: 'my-app',
+    templateUrl: 'app.component.html',
+    providers: [TaskService]
 })
 export class AppComponent {
   title = 'app works!';
