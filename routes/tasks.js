32e3dd175784cceaeaca3243aee5de9d08ff42c4
marcialu <yukimiaka@gmail.com>
2017-05-01 22:26:09 +0200

    Adding Mongojs and DB

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://mar:mar@ds111718.mlab.com:11718/mytasklis', ['tasks']);
 
 router.get('/tasks', function (req, res, next) {
-    res.render('TASK API');
+    db.tasks.find(function (err, tasks) {
+        if (err) {
+            res.send(err);
+        }
+        res.json(tasks);
+    });
 });
 
 module.exports = router;
